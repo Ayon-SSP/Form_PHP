5df1c25b9063a9cd31f6185d6a0a386ceb4aa93c
ayon_ssp <ayon.ssp@gmail.com>
2024-11-24 15:09:47 +0530

    feat: INSERT PHP formated

--- pages/php/insert.php
+++ pages/php/insert.php
@@ -1,38 +1,36 @@
 <?php
-echo "Welcom to the stage where we are ready to get connected to a database<br>";
-// Connecting to the Database
-$servername = "localhost";
-$username = "root";
-$password = "";
-$database = "dbssp1";
-// $database = "for_practice2";
+    echo "Welcom to the stage where we are ready to get connected to a database<br>";
+    // Connecting to the Database
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "dbssp1";
+    // $database = "for_practice2";
 
-$conn = mysqli_connect($servername, $username, $password,$database);
-if(!$conn){
-    die("Sorry we failed to connect: ". mysqli_connect_error());
-}
-else{
-    echo "Connection was successful<br>";
-}
+    $conn = mysqli_connect($servername, $username, $password,$database);
+    if (!$conn) {
+        die("Sorry we failed to connect: ". mysqli_connect_error());
+    } else {
+        echo "Connection was successful<br>";
+    }
 
-$val1 = $_POST['userName'];
-$val2 = $_POST['userPhoneno'];
-$val3 = $_POST['userNum'];
-$val4 = $_POST['userEN'];
+    $val1 = $_POST['userName'];
+    $val2 = $_POST['userPhoneno'];
+    $val3 = $_POST['userNum'];
+    $val4 = $_POST['userEN'];
 
-echo $val1,'<br>';
-echo $val2,'<br>';
-echo $val3,'<br>';
-echo $val4,'<br>';
+    echo $val1,'<br>';
+    echo $val2,'<br>';
+    echo $val3,'<br>';
+    echo $val4,'<br>';
 
-$sql = "INSERT INTO `prac_phpform` (`name`, `phno`, `age`, `enno`) VALUES ('$val1', '$val2', '$val3', '$val4');";
+    $sql = "INSERT INTO `prac_phpform` (`name`, `phno`, `age`, `enno`) VALUES ('$val1', '$val2', '$val3', '$val4');";
 
-// returns 1 if sucess and 0 if not
-$result = mysqli_query($conn, $sql);
-if($result){
-    echo "The record has been inserted successfully!<br>";
-}
-else{
-    echo "The record was not inserted successfully because of this error ---> ". mysqli_error($conn);
-}
+    // returns 1 if sucess and 0 if not
+    $result = mysqli_query($conn, $sql);
+    if ($result) {
+        echo "The record has been inserted successfully!<br>";
+    } else {
+        echo "The record was not inserted successfully because of this error ---> ". mysqli_error($conn);
+    }
 ?> 
